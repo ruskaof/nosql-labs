4ff50df2c2308023adb544577f8239a2f10c752b
ruskaof <199-41@mail.ru>
2026-02-20 12:04:00 +0300

    fix: use env variable for application port

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	appPort, portPresent := os.LookupEnv("APP_PORT")
+
+	if !portPresent {
+		log.Fatal("Environment variable APP_PORT is not present")
+	}
+
 	http.HandleFunc("/health", healthHandler)
-	http.ListenAndServe("0.0.0.0:8080", nil)
+	http.ListenAndServe("0.0.0.0:"+appPort, nil)
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
